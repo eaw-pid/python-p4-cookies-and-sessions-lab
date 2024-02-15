47ad1ac1f669ee4a02dbc84b7d0cd0630050b8dc
Erin Wise <erinwise88@gmail.com>
2024-02-15 12:06:37 -0500

    complete

--- server/app.py
+++ server/app.py
@@ -11,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+
 migrate = Migrate(app, db)
 
 db.init_app(app)
@@ -25,10 +26,24 @@ def index_articles():
 
     pass
 
+##instructions: when a user makes a get request, if this is the first request, 
+#set session['page_views'] to an initial value of 0.
+#for every request, increment session['page_views'] by 1
+#If user has viewed 3 or less pages, render a JSON response with article data
+#else send a message and status code 401
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1
 
-    pass
+    if session['page_views'] <= 3:
+        article = Article.query.filter_by(id=id).first()
+        return make_response(article.to_dict(), 200)
+    
+    return {"message": "Maximum pageview limit reached"}, 401
+        
+
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
